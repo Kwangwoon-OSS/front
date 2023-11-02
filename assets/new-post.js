af96d1aad95176f5dbdf15c8a1fe11bcde284c24
Youngbae1126 <rcplaza2663@gmail.com>
2023-11-03 01:08:00 +0900

    Feat: drop menu 수정

--- assets/new-post.js
+++ assets/new-post.js
@@ -1,17 +1,25 @@
-const optionMenu = document.querySelector('.select__menu'),
-	selectBtn = optionMenu.querySelector('select__btn'),
-	category__btn__text = optionMenu.querySelector('.category__btn__text');
+const optionMenu = document.querySelector('.select__menu');
+const selectBtn = document.getElementById('menu__list');
+const category__btn__text = document.querySelector('.category__btn__text');
+const optionList = document.getElementById('option__list');
 
-selectBtn?.addEventListener('click', () =>
-	optionMenu.classList.toggle('active'),
-);
+selectBtn.addEventListener('click', () => {
+	optionMenu.classList.toggle('active');
+});
+
+optionList.addEventListener('click', () => {
+	optionList.classList.toggle('click');
+});
 
 let option = optionMenu.querySelector('.category__options').children;
 let options = Array.from(option);
+
 options.forEach((option) => {
 	option.addEventListener('click', () => {
 		let selectedOption = option.querySelector('.option__text').innerText;
 		category__btn__text.innerText = selectedOption;
-		console.log(selectedOption);
+		//console.log(selectedOption);
+		optionMenu.classList.remove('active');
+		optionList.classList.remove('click');
 	});
 });
