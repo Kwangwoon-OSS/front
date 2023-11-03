8afaf720a666593cbcf14a67bd15915ef76429b4
Youngbae1126 <rcplaza2663@gmail.com>
2023-11-03 18:14:14 +0900

    Design: 게시글 기본 정보 입력란 디자인 완성

--- assets/new-post.js
+++ assets/new-post.js
@@ -3,16 +3,64 @@ const selectBtn = document.getElementById('menu__list');
 const category__btn__text = document.querySelector('.category__btn__text');
 const optionList = document.getElementById('option__list');
 
+const semesterMenu = document.querySelector('.semester__menu');
+const semesterBtn = document.getElementById('semester__list');
+const semesterBtnText = document.querySelector('.semester__btn__text');
+const semesterOptionList = document.getElementById('semester__option__list');
+
+const departmentMenu = document.querySelector('.department__menu');
+const departmentBtn = document.getElementById('department__list');
+const departmentBtnText = document.querySelector('.department__btn__text');
+const departmentOptionList = document.getElementById(
+	'department__option__list',
+);
+
+const contactMenu = document.querySelector('.contact__menu');
+const contactBtn = document.getElementById('contact__list');
+const contactBtnText = document.querySelector('.contact__btn__text');
+const contactOptionList = document.getElementById('contact__option__list');
+
 selectBtn.addEventListener('click', () => {
 	optionMenu.classList.toggle('active');
 });
-
 optionList.addEventListener('click', () => {
 	optionList.classList.toggle('click');
 });
 
+semesterBtn.addEventListener('click', () => {
+	semesterMenu.classList.toggle('active');
+});
+semesterOptionList.addEventListener('click', () => {
+	semesterOptionList.classList.toggle('click');
+});
+
+departmentBtn.addEventListener('click', () => {
+	departmentMenu.classList.toggle('active');
+});
+departmentOptionList.addEventListener('click', () => {
+	departmentOptionList.classList.toggle('click');
+});
+
+contactBtn.addEventListener('click', () => {
+	contactMenu.classList.toggle('active');
+});
+contactOptionList.addEventListener('click', () => {
+	contactOptionList.classList.toggle('click');
+});
+
 let option = optionMenu.querySelector('.category__options').children;
 let options = Array.from(option);
+
+let semesterOption = semesterMenu.querySelector('.semester__options').children;
+let semesterOptions = Array.from(semesterOption);
+
+let departmentOption = departmentMenu.querySelector(
+	'.department__options',
+).children;
+let departmentOptions = Array.from(departmentOption);
+
+let contactOption = contactMenu.querySelector('.contact__options').children;
+let contactOptions = Array.from(contactOption);
 
 options.forEach((option) => {
 	option.addEventListener('click', () => {
@@ -21,5 +69,38 @@ options.forEach((option) => {
 		//console.log(selectedOption);
 		optionMenu.classList.remove('active');
 		optionList.classList.remove('click');
+	});
+});
+
+semesterOptions.forEach((semesterOption) => {
+	semesterOption.addEventListener('click', () => {
+		let selectedSemesterOption = semesterOption.querySelector(
+			'.semester__option__text',
+		).innerText;
+		semesterBtnText.innerText = selectedSemesterOption;
+		semesterMenu.classList.remove('active');
+		semesterOptionList.classList.remove('click');
+	});
+});
+
+departmentOptions.forEach((departmentOption) => {
+	departmentOption.addEventListener('click', () => {
+		let selectedDepartmentOption = departmentOption.querySelector(
+			'.department__option__text',
+		).innerText;
+		departmentBtnText.innerText = selectedDepartmentOption;
+		departmentMenu.classList.remove('active');
+		departmentOptionList.classList.remove('click');
+	});
+});
+
+contactOptions.forEach((contactOption) => {
+	contactOption.addEventListener('click', () => {
+		let selectedContactOption = contactOption.querySelector(
+			'.contact__option__text',
+		).innerText;
+		contactBtnText.innerText = selectedContactOption;
+		contactMenu.classList.remove('active');
+		contactOptionList.classList.remove('click');
 	});
 });
