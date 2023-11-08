79071597968fa9f791fb38f6dcb97a210ee2d6d1
Youngbae1126 <rcplaza2663@gmail.com>
2023-11-09 00:20:06 +0900

    Test: json-server api test

--- assets/api/test.js
+++ assets/api/test.js
@@ -8,14 +8,18 @@
 // }
 
 function postData() {
-	axios({
-		method: 'get',
-		url: 'http://together-env.eba-idjepbda.ap-northeast-2.elasticbeanstalk.com/posts',
-	})
-		.then(function (response) {
-			console.log(response);
-		})
-		.catch(function (error) {
-			console.log(error);
-		});
+	// axios({
+	// 	method: 'get',
+	// 	url: 'http://localhost:3000/posts',
+	// })
+	// 	.then(function (response) {
+	// 		console.log(response);
+	// 	})
+	// 	.catch(function (error) {
+	// 		console.log(error);
+	// 	});
+
+	fetch('http://localhost:3000/posts')
+		.then((response) => response.json())
+		.then((data) => console.log(data));
 }
