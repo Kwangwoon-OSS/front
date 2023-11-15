0b68eab02c7d1d4f3ecbbe5e4354c0966f00be5b
bjPark98 <113542209+bjPark98@users.noreply.github.com>
2023-11-15 17:12:31 +0900

    토큰자리 추가만

--- assets/profile_modified.js
+++ assets/profile_modified.js
@@ -1,7 +1,10 @@
+const host = window.location.hostname ==='127.0.0.1' ? baseURL:'/api';
+
 function save() {
     var departmentId = 1;
     var nickname = document.getElementById("nickname").value;
     var introduction = document.getElementById("introduction").value;
+    //const token = localStorage.getItem('accessToken');
 
     // 여기에서 서버로 데이터를 전송하거나 필요한 동작을 수행
   const userData = {
@@ -14,15 +17,16 @@ function save() {
 const requestOptions = {
     method: "POST",
     headers: {
+        //"Authorization": token, Bearer 토큰 추가
         "Content-Type": "application/json; charset=UTF-8"
     },
     body: JSON.stringify(userData)
 };
 
-fetch("http://together-env.eba-idjepbda.ap-northeast-2.elasticbeanstalk.com/users/profile", requestOptions)
+fetch(host+'/users/profile', requestOptions)
     .then(response => {
         if (response.status === 201) {
-            // 회원가입 성공
+            // 프로필 수정
             console.log("프로필 저장 완료");
             alert("프로필이 저장되었습니다!");
             window.location.href = "../index.html";
