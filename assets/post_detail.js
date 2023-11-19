1961505a0123491e1b38dade2a15d898c3874561
bjPark98 <113542209+bjPark98@users.noreply.github.com>
2023-11-19 13:42:01 +0900

    커밋 실수 수정

--- assets/post_detail.js
+++ assets/post_detail.js
@@ -12,7 +12,42 @@ const access_token = window.localStorage.getItem('accessToken');
 const postID = window.localStorage.getItem('selectID');
 console.log(postID);
 
+// const style = () => {
+// 	console.log('style');
+// 	document.querySelector('.fa-bookmark').classList.add('fa-solid');
+// };
+
 document.body.addEventListener('load', getPostData());
+
+// 북마크 정보 가져오기
+async function getBook(selectID) {
+	return fetch(host + '/posts/interestin', {
+		method: 'GET',
+		headers: {
+			Authorization: access_token,
+			'Content-Type': 'application/json;charset=UTF-8',
+		},
+	})
+		.then((res) => res.json())
+		.then((data) => {
+			return data;
+			console.log(data);
+			console.log(selectID);
+			// 북마크 정보 중에 현재 게시글이 있는 지 확인하기
+			let bookDataLen = data.length;
+			for (let i = 0; i < bookDataLen; i++) {
+				if (selectID == data[i].id) {
+					// style();
+					// console.log('ok');
+					// document.querySelector('.fa-bookmark').classList.add('fa-solid');
+				}
+				// else {
+				// return 1;
+				// document.querySelector('.fa-bookmark').classList.remove('fa-solid');
+				// }
+			}
+		});
+}
 
 // 작성자 이름 가져오기
 const getWriter = function (userID) {
@@ -68,15 +103,6 @@ const getSubjectData = function (subjectID) {
 			}
 		});
 };
-
-// async function book() {
-// 	fetch(host + '/posts/interestin')
-// 		.then((res) => res.json())
-// 		.then((data) => {
-// 			console.log(data);
-// 		});
-// }
-// book();
 
 async function getPostData() {
 	// window.localStorage.setItem('access_token', access_token);
@@ -135,6 +161,102 @@ async function getPostData() {
 				getConnectUser().then((userData) => {
 					const connectUser = userData;
 
+					getBook(postID).then((result) => {
+						const isBookSelect = document.querySelector('.fa-bookmark');
+						for (let i = 0; i < result.length; i++) {
+							if (postID == result[i].id) {
+								console.log('zero');
+								isBookSelect.classList.add('fa-solid');
+								// console.log(isBookSelect.classList);
+							}
+						}
+
+						// 북마크
+						let clickNum = 0;
+						console.log(clickNum);
+						document
+							.getElementById('bookmark__icon')
+							.addEventListener('click', () => {
+								const postID = window.localStorage.getItem('selectID');
+								const isBookSelect = document.querySelector('.fa-bookmark');
+								const bookClassListLen = isBookSelect.classList.length;
+								console.log(bookClassListLen);
+								console.log(`후 -> ${clickNum}`);
+
+								if (clickNum == 0 && bookClassListLen < 4) {
+									document
+										.querySelector('.fa-bookmark')
+										.classList.add('fa-solid');
+									// 북마크 등록
+									fetch(host + '/posts/' + postID + '/interest', {
+										method: 'POST',
+										headers: {
+											'Content-Type': 'application/json',
+											Authorization: access_token,
+										},
+									}).then((res) => {
+										if (res.ok) {
+											alert('북마크 등록되었습니다.');
+										}
+									});
+									clickNum++;
+								} else if (clickNum < 0) {
+									document
+										.querySelector('.fa-bookmark')
+										.classList.add('fa-solid');
+									// 북마크 등록
+									fetch(host + '/posts/' + postID + '/interest', {
+										method: 'POST',
+										headers: {
+											'Content-Type': 'application/json',
+											Authorization: access_token,
+										},
+									}).then((res) => {
+										if (res.ok) {
+											alert('북마크 등록되었습니다.');
+										}
+									});
+									clickNum++;
+								} else if (clickNum > 0) {
+									document
+										.querySelector('.fa-bookmark')
+										.classList.remove('fa-solid');
+									clickNum--;
+
+									// 북마크 해제
+									fetch(host + '/posts/' + postID + '/interest', {
+										method: 'DELETE',
+										headers: {
+											'Content-Type': 'application/json',
+											Authorization: access_token,
+										},
+									}).then((res) => {
+										if (res.ok) {
+											alert('북마크 해제되었습니다.');
+										}
+									});
+								} else if (clickNum == 0 && bookClassListLen == 4) {
+									document
+										.querySelector('.fa-bookmark')
+										.classList.remove('fa-solid');
+									clickNum--;
+
+									// 북마크 해제
+									fetch(host + '/posts/' + postID + '/interest', {
+										method: 'DELETE',
+										headers: {
+											'Content-Type': 'application/json',
+											Authorization: access_token,
+										},
+									}).then((res) => {
+										if (res.ok) {
+											alert('북마크 해제되었습니다.');
+										}
+									});
+								}
+							});
+					});
+
 					if (connectUser === writer) {
 						console.log('유저가 같습니다.');
 						// 유저가 같으면 수정하기, 삭제하기 버튼 보이기
@@ -181,46 +303,6 @@ deleteBtn.addEventListener('click', () => {
 			});
 	} else {
 		setInterval((window.location.href = '../index.html'), 1000);
-	}
-});
-
-// 북마크
-let clickNum = 0;
-document.getElementById('bookmark__icon').addEventListener('click', () => {
-	const postID = window.localStorage.getItem('selectID');
-
-	if (clickNum == 0) {
-		document.querySelector('.fa-bookmark').classList.add('fa-solid');
-		clickNum++;
-
-		// 북마크 등록
-		fetch(host + '/posts/' + postID + '/interest', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-				Authorization: access_token,
-			},
-		}).then((res) => {
-			if (res.ok) {
-				alert('북마크 등록되었습니다.');
-			}
-		});
-	} else {
-		document.querySelector('.fa-bookmark').classList.remove('fa-solid');
-		clickNum--;
-
-		// 북마크 해제
-		fetch(host + '/posts/' + postID + '/interest', {
-			method: 'DELETE',
-			headers: {
-				'Content-Type': 'application/json',
-				Authorization: access_token,
-			},
-		}).then((res) => {
-			if (res.ok) {
-				alert('북마크 해제되었습니다.');
-			}
-		});
 	}
 });
 
@@ -278,7 +360,6 @@ function displayComments(comments) {
 		const commentShow = document.createElement('div');
 		commentShow.classList.add('comment__show');
 
-		
 		const userDiv = document.createElement('div');
 		getConnectUser().then((data) => {
 			const userName = data;
@@ -299,18 +380,6 @@ function displayComments(comments) {
 		const lineDiv = document.createElement('div');
 		lineDiv.classList.add('line');
 
-		const deleteButton = document.createElement('button');
-		deleteButton.textContent = '삭제';
-
-		// 댓글 작성자와 현재 사용자가 동일한 경우에만 삭제 버튼 표시
-		if (comments.username === currentUser.nickname) {
-			deleteButton.style.display = 'inline-block';
-		}
-
-		// 삭제 버튼을 눌렀을 때 댓글 삭제 함수 호출
-		deleteButton.addEventListener('click', () => {
-			deleteComment(comments.postId, comments.commentId);
-		});
 		commentShow.appendChild(userDiv);
 		commentShow.appendChild(contentDiv);
 		commentShow.appendChild(dateDiv);
@@ -323,21 +392,17 @@ function displayComments(comments) {
 //댓글 등록하기
 function post_save() {
 	var content = document.getElementById('comment__input').value;
+	var parentId = localStorage.getItem('selectId');
 	var used = 'Y';
-	const access_token = localStorage.getItem('accessToken');
+	//const token = localStorage.getItem('accessToken');
 
-	// 사용자 정보 가져오기
-    getConnectUser().then(userData => {
-        // 여기에서 userData.id를 사용하여 작업 수행
-        const userID = userData.id;
-
-        // 서버로 데이터를 전송하거나 필요한 동작을 수행
-        const commentData = {
-            content: content,
-            parentId: userID,
-            used: used,
-            // bearer 토큰 추가
-        };
+	// 여기에서 서버로 데이터를 전송하거나 필요한 동작을 수행
+	const userData = {
+		content: content,
+		parentId: parentId,
+		used: used,
+		//bearer토큰 추가해야함
+	};
 
 	const requestOptions = {
 		method: 'POST',
@@ -345,48 +410,23 @@ function post_save() {
 			Authorization: access_token,
 			'Content-Type': 'application/json; charset=UTF-8',
 		},
-		body: JSON.stringify(commentData),
+		body: JSON.stringify(userData),
 	};
 
-	fetch(host + `/posts/${postID}/comment`, requestOptions)
+	fetch(host + `/posts/profile/${postID}/comment`, requestOptions)
 		.then((response) => {
 			if (response.status === 200) {
 				//댓글 작성 완료
 				alert('댓글이 작성되었어요!');
 			} else {
-				// 댓글 작성 실패 또는 다른 상태 코드
+				// 회원가입 실패 또는 다른 상태 코드
 				console.error('댓글 실패. 상태 코드: ' + response.status);
 			}
 		})
 		.catch((error) => {
 			console.error('요청 실패:', error);
 		});
-	});
 }
-
-// 댓글 삭제 함수
-function deleteComment(postId, commentId) {
-        fetch(`/api/posts/${postId}/${commentId}`, {
-            method: 'DELETE',
-            headers: {
-                Authorization: access_token,
-            },
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('댓글을 삭제할 수 없습니다.');
-            }
-            // 삭제가 성공하면 댓글 목록을 다시 로드하여 업데이트
-            fetchComments(); // 댓글을 다시 로드하는 함수 호출
-        })
-        .catch(error => {
-            console.error('댓글 삭제 중 오류가 발생했습니다:', error);
-            // 오류 처리
-        });
-    }
-
-	//댓글 버튼 숨겼다가 보이게 하는 기능 
-
 
 // 페이지 로드 시 댓글을 가져오도록 설정
 document.addEventListener('DOMContentLoaded', fetchComments);
