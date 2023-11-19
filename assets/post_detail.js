ebb728ef441abc37383c8eb805aea3490088d584
bjPark98 <113542209+bjPark98@users.noreply.github.com>
2023-11-20 02:27:13 +0900

    Update post_detail.js

--- assets/post_detail.js
+++ assets/post_detail.js
@@ -360,10 +360,8 @@ function displayComments(comments) {
 
 		//삭제 버튼이 작성자만 보이게
 		getConnectUser().then((userData) => {
-			const currentUser = userData; // 현재 사용자의 ID
+			const currentUser = userData.nickname; // 현재 사용자의 ID
 			const commentWriter = comments.username; // 댓글 작성자의 ID
-			console.log(currentUser);
-			console.log(commentWriter);
 
 			if (commentWriter !== currentUser) {
 				deleteButton.style.display = 'none'; // 숨기기
